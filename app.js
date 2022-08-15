d076066b4d979f955043547f93ecab4d7f8ae713
Sahand <32298292+sahandhabibi@users.noreply.github.com>
2022-08-15 22:01:50 +0430

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ async function updateBlockbookglobalMetrics(){
           }
          };    
    
-        const blockbookGlobalLatestBlock = await axios.get(blockbookGlobalScanApiUrl,{headers: {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}}, {proxy});
+        const blockbookGlobalLatestBlock = await axios.get(blockbookGlobalScanApiUrl,{headers: {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}, proxy});
     
         console.log('done blockbookGlobalLatestBlock');
         console.log('///////////////////////////////');
